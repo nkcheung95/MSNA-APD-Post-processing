b6d91f86ee6df6a7a7f0d3192e2d02fd5b300e39
nkcheung <124935202+nkcheung95@users.noreply.github.com>
2024-09-03 14:04:12 -0600

    Update dbscan_script.R
    added latency summary output

--- dbscan_script.R
+++ dbscan_script.R
@@ -422,7 +422,12 @@ if (!is.null(raw_files) && length(raw_files) > 0) {
     sort_split_clusters[[value]] <- subset_df
   }
   
-  
+  #####AP DBSCAN mean_latency summary
+df_mean_ap_latency <- do.call(rbind, dbscan_split_clusters_list)
+mean_ap_latency <- test_dataframe %>%
+group_by(`Burst Number`,`Cluster Number`, neuron_id) %>%  # Group by Cluster Number and neuron_id
+summarise(mean_ap_latency = mean(ap_latency, na.rm = TRUE),sd_ap_latency= sd(ap_latency, na.rm=T))  # Calculate the mean ap_latency
+write.csv(mean_ap_latency,file.path(file_id_folder, paste0(file.id, "_apd_latency_summary.csv")))
   ###SAVE R FILE FOR FUTURE USE
   # Save the R session inside the folder
   save.image(file.path(file_id_folder, paste0(file.id, "_session.RData")))
